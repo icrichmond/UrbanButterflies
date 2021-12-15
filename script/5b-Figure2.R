dc0eb70b608452ee05b9ec6f960650ce1a27e3f7
icrichmond <icrichmond@mun.ca>
2021-12-15 16:28:46 -0500

    Updated top model figure

--- script/5b-Figure2.R
+++ script/5b-Figure2.R
@@ -1,5 +1,5 @@
 #### PACKAGES #### 
-p <- c("ggplot2", "visreg")
+p <- c("ggplot2", "visreg", "patchwork")
 lapply(p, library, character.only = T)
 
 #### DATA #### 
@@ -8,9 +8,23 @@ mod <- readRDS("large/ModAbundanceDist50.rds")
 
 #### PLOT #### 
 # interaction term
-visreg(mod, "PerCut", by="anthroper", overlay=TRUE, band = F, gg = T)
+i <- visreg(mod, "PerCut", by="anthroper", overlay=TRUE, band = F, gg = T) + 
+  geom_point(size = 3, aes(colour=anthroper))+
+  labs(y = "Abundance", x = "Mowed (%)") + 
+  scale_colour_viridis_d(name = "Anthropogenic Land-Use (%)") +
+  theme_classic() + 
+  theme(legend.position = c(0.8, 0.9))
 # interaction indicates that higher anthropogenic percentage the greater the effect of percent mowed on abundance
 
 # main effects 
-visreg(mod) + 
-  facet_wrap(~.)
+p <- visreg(mod, xvar = "PerCut", line.par = list(col = 'black'), gg = T) + 
+  geom_point(size = 2) + 
+  labs(y = "Abundance", x = "Mowed (%)")+
+  theme_classic()
+a <- visreg(mod, xvar = "anthroper", line.par = list(col = 'black'), gg = T) +
+  geom_point(size = 2) + 
+  labs(y = "Abundance", x = "Anthropogenic Land-Use at 50 m (%)") + 
+  theme_classic()
+
+# together 
+i | (p/a)
