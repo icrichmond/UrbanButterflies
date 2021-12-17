17f5599b7cea2e11f1196449546130c6b233556d
Isabella Richmond <icrichmond@mun.ca>
2021-12-17 11:19:02 -0500

    Change disturbance to binary disturbed/undisturbed

--- script/5a-Figure1.R
+++ script/5a-Figure1.R
@@ -17,13 +17,13 @@ ponds <- ponds %>%
 # Note: for this code to work you need a Google API with Geocoding and Maps Static enabled
 register_google(key = "AIzaSyDlRF5BYeskCH7qWtq13WUV5ifG9Q1kT1c")
 qmap(location = "Terry Carisse Park, Ottawa, Ontario", zoom = 10, maptype = "satellite", source = "google") +
-  geom_point(data = ponds, aes(x = lat, y = long, colour = PerCut), size = 4) + 
+  geom_point(data = ponds, aes(x = lat, y = long, colour = Disturbance), size = 3) + 
   coord_sf(crs = st_crs(4326), expand = FALSE) + 
-  scale_colour_viridis_c()+ 
-  labs(colour = "Mowed (%)") +
+  scale_colour_viridis_d()+ 
+  labs(colour = "Disturbance") +
   theme(legend.position = c(0.9, 0.15)) +
   annotation_scale(location = "tl", width_hint = 0.3) +
   annotation_north_arrow(location = "tl", which_north = "true", pad_x = unit(0.2, "in"), pad_y = unit(0.5, "in"), style = north_arrow_fancy_orienteering) 
 
 #### Save ####
-ggsave("figures/StudyArea.jpg", dpi = 450)
+ggsave("figures/StudyArea.jpg", width = 4542, height = 2882, units = "px", dpi = 450)
