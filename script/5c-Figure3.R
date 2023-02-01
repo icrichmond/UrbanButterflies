a7a0742d9cbb6ee10a659c3a094d279aaf3d3eef
Isabella Richmond <icrichmond@mun.ca>
2023-02-01 11:45:29 -0500

    new model figures without mowing

--- script/5c-Figure3.R
+++ script/5c-Figure3.R
@@ -4,90 +4,87 @@ lapply(p, library, character.only = T)
 
 #### DATA #### 
 # species richness models 
-ab <- readRDS("large/AbundanceDistModels.rds")
-ab50 <- ab$AbDist50
 sr <- readRDS("large/SpeciesRichnessDistModels.rds")
+sr2000 <- sr$SRDist2000
 sr1000 <- sr$SRDist1000
+sr5000 <- sr$SRDist5000
+sr500 <- sr$SRDist500
 # shannon models
 sh <- readRDS("large/ShannonDistModels.rds")
 sh1000 <- sh$ShDist1000
+sh2000 <- sh$ShDist2000
+sh500 <- sh$ShDist500
+sh5000 <- sh$ShDist5000
 
 #### PLOT #### 
-## Abundance
-ab50a <- visreg(ab50, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
-  geom_point(size = 2) +
-  labs(y = "Abundance", x = "Urbanization (%)")+
-  theme_classic()
-ab50b <- visreg(ab50, xvar = "Disturbance", line.par = list(col = "black"), gg = T) +
-  labs(y = "Abundance", x = "")+
-  theme_classic()
-ab50c <- visreg(ab50, xvar = "anthroper", by= "Disturbance", overlay=TRUE, band = T, gg = T) +
-  geom_point(size = 3, aes(colour = Disturbance))+
-  labs(y = "Abundance", x = "Urbanization (%)") + 
-  scale_colour_viridis_d(name = "Disturbance") +
-  scale_fill_viridis_d(name = "Disturbance", alpha = 0.3)+
-  theme_classic() + 
-  theme(legend.position = c(0.8, 0.9))
-
-p0 <- (ab50a / ab50b) | ab50c + plot_annotation(tag_levels = 'A')
-
 ## Species Richness 
+sr2000a <- visreg(sr2000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
+  geom_point(size = 2) +
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sr2000)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sr2000)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sr2000)$adj.r.squared, 2)))) + 
+  labs(y = "Species Richness", x = "Urbanization (%) at 2,000 m")+
+  theme_classic()
+
 sr1000a <- visreg(sr1000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
   geom_point(size = 2) +
-  labs(y = "Species Richness", x = "Urbanization (%)")+
-  theme_classic()
-sr1000m <- visreg(sr1000, xvar = "Disturbance", line.par = list(col = "black"), gg = T) +
-  labs(y = "Species Richness", x = "")+
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sr1000)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sr1000)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sr1000)$adj.r.squared, 2)))) + 
+  labs(y = "", x = "Urbanization (%) at 1,000 m")+
   theme_classic()
 
-p1 <- sr1000m + sr1000a + plot_annotation(tag_levels = 'A')
+sr5000a <- visreg(sr5000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
+  geom_point(size = 2) +
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sr5000)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sr5000)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sr5000)$adj.r.squared, 2)))) + 
+  labs(y = "Species Richness", x = "Urbanization (%) at 5,000 m")+
+  theme_classic()
+
+sr500a <- visreg(sr500, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
+  geom_point(size = 2) +
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sr500)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sr500)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sr500)$adj.r.squared, 2)))) + 
+  labs(y = "", x = "Urbanization (%) at 500 m")+
+  theme_classic()
+
+p1 <- (sr2000a + sr1000a)/(sr5000a + sr500a) + plot_annotation(tag_levels = 'A')
 
 ## Shannon Diversity
-sh1000m <- visreg(sh1000, xvar = "Disturbance", line.par = list(col = "black"), gg = T) +
-  labs(y = "Shannon diversity", x = "")+
-  theme_classic()
 sh1000a <- visreg(sh1000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
   geom_point(size = 2) +
-  labs(y = "Shannon diversity", x = "Urbanization (%)")+
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sh1000)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sh1000)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sh1000)$adj.r.squared, 2)))) + 
+  labs(y = "Shannon Diversity", x = "Urbanization (%) at 1,000 m")+
   theme_classic()
-sh1000i <- visreg(sh1000, xvar = "anthroper", by= "Disturbance", overlay=TRUE, band = T, gg = T) +
-  geom_point(size = 3, aes(colour = Disturbance))+
-  labs(y = "Shannon diversity", x = "Urbanization (%)") + 
-  scale_colour_viridis_d(name = "Disturbance") +
-  scale_fill_viridis_d(name = "Disturbance", alpha = 0.3)+
-  theme_classic() + 
-  theme(legend.position = c(0.8, 0.9))
 
-p3 <- ((sh1000m/sh1000a) | sh1000i) + plot_annotation(tag_levels = 'A')
-
-sh100a <- visreg(sh100, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
+sh2000a <- visreg(sh2000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
   geom_point(size = 2) +
-  labs(y = "Shannon diversity", x = "Urbanization (%)")+
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sh2000)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sh2000)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sh2000)$adj.r.squared, 2)))) + 
+  labs(y = "Shannon Diversity", x = "Urbanization (%) at 2,000 m")+
   theme_classic()
-sh100i <- visreg(sh100, xvar = "anthroper", by= "Disturbance", overlay=TRUE, band = T, gg = T) +
-  geom_point(size = 3, aes(colour = Disturbance))+
-  labs(y = "Shannon diversity", x = "Urbanization (%)") + 
-  scale_colour_viridis_d(name = "Disturbance") +
-  scale_fill_viridis_d(name = "Disturbance", alpha = 0.3)+
-  theme_classic() + 
-  theme(legend.position = c(0.8, 0.9))
 
-p4 <- sh1000a + sh1000i + plot_annotation(tag_levels = 'A')
+sh500a <- visreg(sh500, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
+  geom_point(size = 2) +
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sh500)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sh500)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sh500)$adj.r.squared, 2)))) + 
+  labs(y = "Shannon Diversity", x = "Urbanization (%) at 500 m")+
+  theme_classic()
+
+sh5000a <- visreg(sh5000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
+  geom_point(size = 2) +
+  annotate("text", x = 0.6, y = 10, label = bquote("Estimate = " ~ .(round(summary(sh5000)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(sh5000)$coefficients[2,2], 2)))) + 
+  annotate("text", x = 0.6, y = 9.5, label = bquote("Adj." ~ R^2 ~ " = " ~ .(round(summary(sh5000)$adj.r.squared, 2)))) + 
+  labs(y = "Shannon Diversity", x = "Urbanization (%) at 5,000 m")+
+  theme_classic()
+
+p2 <- (sh1000a + sh2000a)/(sh500a + sh5000a) + plot_annotation(tag_levels = 'A')
 
 
 #### SAVE ####
-png("figures/AbundanceModels.png", width = 6000, height = 5000, units = "px", res = 1000)
-p0
-dev.off()
-
-png("figures/SpeciesRichnessModels.png", width = 6000, height = 3000, units = "px", res = 1000)
+png("figures/SpeciesRichnessModels.png", width = 8000, height = 7000, units = "px", res = 1000)
 p1
 dev.off()
 
-png("figures/Shannon1000mModel.png", width = 6000, height = 5000, units = "px", res = 1000)
-p3
-dev.off()
-
-png("figures/Shannon100mModel.png", width = 6000, height = 3000, units = "px", res = 1000)
-p4
+png("figures/ShannonDiversityModels.png", width = 8000, height = 7000, units = "px", res = 1000)
+p2
 dev.off()
