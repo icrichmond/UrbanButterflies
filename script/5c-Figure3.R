1a5a10ac63240d924a9fdf9be4ce8b805ae5d3e6
Isabella Richmond <icrichmond@mun.ca>
2023-02-01 13:00:41 -0500

    abundance figure

--- script/5c-Figure3.R
+++ script/5c-Figure3.R
@@ -3,6 +3,9 @@ p <- c("ggplot2", "visreg", "patchwork")
 lapply(p, library, character.only = T)
 
 #### DATA #### 
+# abundance model
+ab <- readRDS("large/AbundanceTopModels.rds")
+ab50 <- ab$AbDist50
 # species richness models 
 sr <- readRDS("large/SpeciesRichnessDistModels.rds")
 sr2000 <- sr$SRDist2000
@@ -17,6 +20,29 @@ sh500 <- sh$ShDist500
 sh5000 <- sh$ShDist5000
 
 #### PLOT #### 
+## Abundance
+ab50a <- visreg(ab50, xvar = "nspecies", band = FALSE, line.par = list(col = "white"), gg = T) +
+  geom_point(size = 2) + 
+  annotate("text", x = 25, y = 100, label = bquote("Estimate = " ~ .(round(summary(ab50)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(ab50)$coefficients[2,2], 2)))) + 
+  labs(y = "Abundance", x = "Number of Flowering Species")+
+  theme_classic()
+
+ab50b <- visreg(ab50, xvar = "avgbloom", band = FALSE, line.par = list(col = "white"), gg = T) +
+  geom_point(size = 2) + 
+  annotate("text", x = 15, y = 125, label = bquote("Estimate = " ~ .(round(summary(ab50)$coefficients[3,1], 2)) ~ "+/-" ~ .(round(summary(ab50)$coefficients[3,2], 2)))) + 
+  labs(y = "Abundance", x = "Average % Blooming Cover")+
+  theme_classic()
+
+ab50c <- visreg(ab50, xvar = "nspecies", by = "avgbloom", overlay = T, band = FALSE, 
+                line.par = list(col = "white", alpha = 0), 
+                points.par = list(size = 2), gg = T) +
+  scale_color_viridis_d() + 
+  annotate("text", x = 25, y = 125, label = bquote("Estimate = " ~ .(round(summary(ab50)$coefficients[4,1], 2)) ~ "+/-" ~ .(round(summary(ab50)$coefficients[4,2], 2)))) + 
+  labs(y = "Abundance", x = "Number of Flowering Species", colour = "Average % Blooming Cover") +
+  theme_classic()
+
+p1 <- (ab50a/ab50b)|ab50c
+
 ## Species Richness 
 sr2000a <- visreg(sr2000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
   geom_point(size = 2) +
@@ -46,7 +72,7 @@ sr500a <- visreg(sr500, xvar = "anthroper", line.par = list(col = "black"), gg =
   labs(y = "", x = "Urbanization (%) at 500 m")+
   theme_classic()
 
-p1 <- (sr2000a + sr1000a)/(sr5000a + sr500a) + plot_annotation(tag_levels = 'A')
+p2 <- (sr2000a + sr1000a)/(sr5000a + sr500a) + plot_annotation(tag_levels = 'A')
 
 ## Shannon Diversity
 sh1000a <- visreg(sh1000, xvar = "anthroper", line.par = list(col = "black"), gg = T) +
@@ -77,14 +103,19 @@ sh5000a <- visreg(sh5000, xvar = "anthroper", line.par = list(col = "black"), gg
   labs(y = "Shannon Diversity", x = "Urbanization (%) at 5,000 m")+
   theme_classic()
 
-p2 <- (sh1000a + sh2000a)/(sh500a + sh5000a) + plot_annotation(tag_levels = 'A')
+p3 <- (sh1000a + sh2000a)/(sh500a + sh5000a) + plot_annotation(tag_levels = 'A')
 
 
 #### SAVE ####
-png("figures/SpeciesRichnessModels.png", width = 8000, height = 7000, units = "px", res = 1000)
+png("figures/AbundanceModels.png", width = 10000, height = 8000, units = "px", res = 1000)
 p1
 dev.off()
 
-png("figures/ShannonDiversityModels.png", width = 8000, height = 7000, units = "px", res = 1000)
+
+png("figures/SpeciesRichnessModels.png", width = 8000, height = 7000, units = "px", res = 1000)
 p2
+dev.off()
+
+png("figures/ShannonDiversityModels.png", width = 8000, height = 7000, units = "px", res = 1000)
+p3
 dev.off()
