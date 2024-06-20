0588e4bb28937abe5104c7a511170dfd0e7921e7
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2024-06-20 12:34:37 -0400

    map packages

--- script/0-Packages.R
+++ script/0-Packages.R
@@ -2,5 +2,5 @@
 p <- c("data.table", "readr", "dplyr", "tidyr", "stringr", 
        "tibble", "purrr", "sf", "iNEXT", "ggplot2", "vegan",
        "lme4", "lmerTest", "marginaleffects", "performance",
-       "osmdata", "ggmap", "ggspatial")
+       "osmdata", "ggmap", "ggspatial", "patchwork")
 lapply(p, library, character.only = T)
