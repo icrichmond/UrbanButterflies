866a2c8a0fdbd89d44fdfe922a7fb60c68f58560
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2024-07-03 18:53:51 -0400

    add two buffer distances and niche breadth SR

--- script/4a-UrbanizationModels.R
+++ script/4a-UrbanizationModels.R
@@ -11,23 +11,41 @@ ab <- inner_join(anthro, butt, by = join_by("Pond" == "SWP"))
 
 # Model -------------------------------------------------------------------
 
-mod_ab <- lmer(Abundance ~ 1 + (1 + anthroper | Niche.Breadth), data = an)
-mod_sh <- lm(Shannon ~ anthroper, data = ab)
-mod_sr <- lm(SpeciesRichness ~ anthroper, data = ab)
+mod_ab_50 <- glmer(Abundance ~ 1 + anthroper_50 + (1 + anthroper_50 | Niche.Breadth), family = poisson(), data = an)
+mod_n_sr_50 <- glmer(Species.Richness ~ 1 + anthroper_50 + (1 + anthroper_50 | Niche.Breadth), family = poisson(), data = an)
+mod_sh_50 <- lm(Shannon ~ anthroper_50, data = ab)
+mod_sr_50 <- lm(SpeciesRichness ~ anthroper_50, data = ab)
+
+mod_ab_400 <- glmer(Abundance ~ 1 + anthroper_400 + (1 + anthroper_400 | Niche.Breadth), family = poisson(), data = an)
+mod_n_sr_400 <- glmer(Species.Richness ~ 1 + anthroper_400 +(1 + anthroper_400 | Niche.Breadth), family = poisson(), data = an)
+mod_sh_400 <- lm(Shannon ~ anthroper_400, data = ab)
+mod_sr_400 <- lm(SpeciesRichness ~ anthroper_400, data = ab)
 
 # Diagnostics -------------------------------------------------------------
 
 source('script/function-ResidPlots.R')
 
 pdf('figures/diagnostics/UrbanizationModels.pdf')
-resid_plots(mod_ab, "Abundance")
-resid_plots(mod_sh, "Shannon")
-resid_plots(mod_sr, "Species Richness")
+resid_plots(mod_ab_50, "Abundance @ 50 m")
+resid_plots(mod_n_sr_50, "Niche Species Richness @ 50 m")
+resid_plots(mod_sh_50, "Shannon @ 50 m")
+resid_plots(mod_sr_50, "Species Richness @ 50 m")
+
+resid_plots(mod_ab_400, "Abundance @ 400 m")
+resid_plots(mod_n_sr_400, "Niche Species Richness @ 400 m")
+resid_plots(mod_sh_400, "Shannon @ 400 m")
+resid_plots(mod_sr_400, "Species Richness @ 400 m")
 dev.off()
 
 
 # Save --------------------------------------------------------------------
 
-saveRDS(mod_ab, 'large/UrbAbund.rds')
-saveRDS(mod_sh, 'large/UrbShann.rds')
-saveRDS(mod_sr, 'large/UrbSR.rds')
+saveRDS(mod_ab_50, 'large/UrbAbund_50.rds')
+saveRDS(mod_n_sr_50, 'large/UrbNicheSR_50.rds')
+saveRDS(mod_sh_50, 'large/UrbShann_50.rds')
+saveRDS(mod_sr_50, 'large/UrbSR_50.rds')
+
+saveRDS(mod_ab_400, 'large/UrbAbund_400.rds')
+saveRDS(mod_n_sr_400, 'large/UrbNicheSR_400.rds')
+saveRDS(mod_sh_400, 'large/UrbShann_400.rds')
+saveRDS(mod_sr_400, 'large/UrbSR_400.rds')
