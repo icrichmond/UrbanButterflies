00410f09b8dab3a292afded0513ea0cc53d3be87
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2024-08-09 12:31:40 -0400

    forcats

--- script/0-Packages.R
+++ script/0-Packages.R
@@ -1,6 +1,6 @@
 # Packages ----------------------------------------------------------------
 p <- c("data.table", "readr", "dplyr", "tidyr", "stringr", 
        "tibble", "purrr", "sf", "iNEXT", "ggplot2", "vegan",
-       "sjPlot", "marginaleffects", "performance",
+       "sjPlot", "marginaleffects", "performance", "forcats",
        "osmdata", "ggmap", "ggspatial", "patchwork")
 lapply(p, library, character.only = T)
