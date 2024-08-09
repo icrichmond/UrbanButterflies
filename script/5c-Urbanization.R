8b96aaa79016420bf72a4db9a2ce3cc26d90501f
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2024-08-09 15:04:17 -0400

    update plots with new models

--- script/5c-Urbanization.R
+++ script/5c-Urbanization.R
@@ -7,7 +7,7 @@ plant <- read.csv('output/PlantCleanbySite.csv') %>%
   mutate(Pond = str_remove(Pond, "SWF-"),
          Pond = as.numeric(Pond))
 anthro <- st_read("output/AnthroFull.gpkg")
-niche <- read.csv('input/ButterflyNicheBreadth.csv')
+niche <- read.csv('output/ButterflyNiche.csv')
 butt <- read.csv('output/ButterflyCleanbySite.csv')
 
 an <- left_join(niche, anthro, by = "Pond")
@@ -29,30 +29,10 @@ urb_sh_400 <- readRDS('large/UrbShann_400.rds')
 ab_an <- basic_plot(urb_ab_400, condition = "anthroper_400", 
            dat = anp, x = anthroper_400, y = Abundance,
            xlab = "Anthropogenic Land Cover (%)", ylab = "Butterfly Abundance") + 
-  annotate("text", x = 0.8, y = 95, label = bquote("IRR = " ~ .(round(exp(summary(urb_ab_400)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(urb_ab_400)$coefficients[2,2]), 2)))) + 
-  annotate("text", x = 0.8, y = 90, label = bquote("p-value = " ~ .(round(summary(urb_ab_400)$coefficients[2,4], 4)))) +
-  annotate("text", x = 0.8, y = 85, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(urb_ab_400), 1 - deviance/null.deviance)), 2)))) 
+  annotate("text", x = 0.75, y = 105, label = bquote("IRR = " ~ .(round(exp(summary(urb_ab_400)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(urb_ab_400)$coefficients[2,2]), 2)))) + 
+  annotate("text", x = 0.75, y = 101, label = bquote("p-value = " ~ .(round(summary(urb_ab_400)$coefficients[2,4], 4)))) +
+  annotate("text", x = 0.75, y = 97, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(urb_ab_400), 1 - deviance/null.deviance)), 2)))) 
 
-  
-
-ab_ni <- basic_plot(urb_ab_400, condition = c("anthroper_400", "Niche.Breadth"), dat = anp, 
-           x = anthroper_400, y = Abundance, colour = Niche.Breadth,
-           xlab = "Anthropogenic Land Cover (%)", ylab = "Butterfly Abundance") + 
-  annotate("text", x = 0.8, y = 95, label = bquote("IRR (WA) = " ~ .(round(exp(summary(urb_ab_400)$coefficients[3,1]), 2)) ~ "+/-" ~ .(round(exp(summary(urb_ab_400)$coefficients[3,2]), 2)))) + 
-  annotate("text", x = 0.8, y = 90, label = bquote("p-value = " ~ .(round(summary(urb_ab_400)$coefficients[3,4], 14)))) +
-  annotate("text", x = 0.8, y = 85, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(urb_ab_400), 1 - deviance/null.deviance)), 2)))) +
-  scale_colour_viridis_d() + 
-  scale_fill_viridis_d()
-
-
-an_int <- int_plot(urb_ab_400, list("anthroper_400" = 0.5), condition = c("Niche.Breadth"), 
-                   xlab = "", ylab = "Change in butterfly abundance with 50% increase \nin anthropogenic land cover")
-
-
-ab_urb <- (ab_an + ab_ni) / an_int + 
-  plot_annotation(tag_levels = 'a', tag_suffix = ")") & 
-  plot_layout(guides = 'collect') & 
-  theme(legend.position = 'top')
 
 
 # Species Richness 
@@ -60,28 +40,22 @@ ab_urb <- (ab_an + ab_ni) / an_int +
 sr_an <- basic_plot(urb_n_sr_400, condition = "anthroper_400", 
                     dat = anp, x = anthroper_400, y = Species.Richness,
                     xlab = "Anthropogenic Land Cover (%)", ylab = "Butterfly Species Richness") + 
-  annotate("text", x = 0.75, y = 8.5, label = bquote("IRR = " ~ .(round(exp(summary(urb_n_sr_400)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(urb_n_sr_400)$coefficients[2,2]), 2)))) + 
-  annotate("text", x = 0.75, y = 8, label = bquote("p-value = " ~ .(round(summary(urb_n_sr_400)$coefficients[2,4], 3)))) +
-  annotate("text", x = 0.75, y = 7.5, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(urb_n_sr_400), 1 - deviance/null.deviance)), 2)))) 
-
-sr_int <- int_plot(urb_n_sr_400, list("anthroper_400" = 0.5), condition = c("Niche.Breadth"), 
-                   xlab = "", ylab = "Change in butterfly species richness with 50% \nincrease in anthropogenic land cover")
-
+  annotate("text", x = 0.75, y = 15.5, label = bquote("IRR = " ~ .(round(exp(summary(urb_n_sr_400)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(urb_n_sr_400)$coefficients[2,2]), 2)))) + 
+  annotate("text", x = 0.75, y = 15, label = bquote("p-value = " ~ .(round(summary(urb_n_sr_400)$coefficients[2,4], 3)))) +
+  annotate("text", x = 0.75, y = 14.5, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(urb_n_sr_400), 1 - deviance/null.deviance)), 2)))) 
 
 # Shannon
 sh_an <- basic_plot(urb_sh_400, condition = "anthroper_400", 
                     dat = abp, x = anthroper_400, y = Shannon,
                     xlab = "Anthropogenic Land Cover (%)", ylab = "Butterfly Shannon Diversity") + 
   annotate("text", x = 0.75, y = 8.5, label = bquote("IRR = " ~ .(round(summary(urb_sh_400)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(urb_sh_400)$coefficients[2,2], 2)))) + 
-  annotate("text", x = 0.75, y = 8, label = bquote("p-value = " ~ .(round(summary(urb_sh_400)$coefficients[2,4], 3)))) +
-  annotate("text", x = 0.75, y = 7.5, label = bquote(R^2 ~ " = "  ~ .(round(summary(urb_sh_400)$adj.r.squared , 2)))) 
+  annotate("text", x = 0.75, y = 8.25, label = bquote("p-value = " ~ .(round(summary(urb_sh_400)$coefficients[2,4], 3)))) +
+  annotate("text", x = 0.75, y = 8, label = bquote(R^2 ~ " = "  ~ .(round(summary(urb_sh_400)$adj.r.squared , 2)))) 
 
 
-sr_urb <- (sr_an + sr_int) / sh_an + 
-  plot_annotation(tag_levels = 'a', tag_suffix = ")") 
-
-
+urb <- ab_an + sr_an + sh_an + 
+  plot_annotation(tag_levels = 'a', tag_suffix = ')')
+  
 # Save --------------------------------------------------------------------
-ggsave('figures/UrbanizationAbundance.png', ab_urb, height = 10, width = 10, units = 'in', dpi = 450)
-ggsave('figures/UrbanizationSR.png', sr_urb, height = 10, width = 10, units = 'in', dpi = 450)
+ggsave('figures/Urbanization.png', urb, height = 8, width = 10, units = 'in', dpi = 450)
 
--- script/5d-Total.R
+++ script/5d-Total.R
@@ -8,7 +8,7 @@ plant <- read.csv('output/PlantCleanbySite.csv') %>%
   mutate(Pond = str_remove(Pond, "SWF-"),
          Pond = as.numeric(Pond))
 anthro <- st_read("output/AnthroFull.gpkg")
-niche <- read.csv('input/ButterflyNicheBreadth.csv')
+niche <- read.csv('output/ButterflyNiche.csv')
 butt <- read.csv('output/ButterflyCleanbySite.csv')
 
 an <- left_join(niche, anthro, by = "Pond")
@@ -27,30 +27,33 @@ tot_sh <- readRDS('large/TotShann.rds')
 # Total Bloom -------------------------------------------------------------
 
 # Abundance 
-ab_tot_n <- basic_plot(tot_ab, condition = c("avgbloom", "Niche.Breadth"), dat = anp, 
-                       x = avgbloom, y = Abundance, colour = Niche.Breadth,
+ab_tot_avg <- basic_plot(tot_ab, condition = c("avgbloom"), 
+                         dat = anp, x = avgbloom, y = Abundance,
                        xlab = "Average Total Bloom Cover", ylab = "Butterfly Abundance") + 
-  annotate("text", x = 19, y = 95, label = bquote("IRR (WA) = " ~ .(round(exp(summary(tot_ab)$coefficients[4,1]), 2)) ~ "+/-" ~ .(round(exp(summary(tot_ab)$coefficients[4,2]), 2)))) + 
-  annotate("text", x = 19, y = 90, label = bquote("p-value = " ~ .(round(summary(tot_ab)$coefficients[4,4], 4)))) +
-  annotate("text", x = 19, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(tot_ab), 1 - deviance/null.deviance)), 2)))) +
-  scale_colour_viridis_d() + 
-  scale_fill_viridis_d()
+  annotate("text", x = 19, y = 95, label = bquote("IRR = " ~ .(round(exp(summary(tot_ab)$coefficients[3,1]), 2)) ~ "+/-" ~ .(round(exp(summary(tot_ab)$coefficients[3,2]), 2)))) + 
+  annotate("text", x = 19, y = 90, label = bquote("p-value = " ~ .(round(summary(tot_ab)$coefficients[3,4], 21)))) +
+  annotate("text", x = 19, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(tot_ab), 1 - deviance/null.deviance)), 2))))
 
-ab_tot <- int_plot(tot_ab, list("avgbloom" = 10), condition = c("Niche.Breadth"), 
-                   xlab = "", ylab = "Change in butterfly abundance with 10% \nincrease in average total bloom cover")
+
+ab_tot_n <- basic_plot(tot_ab, condition = c("nspecies"), dat = anp,
+                       x = nspecies, y = Abundance,
+                       xlab = "Number of Flowering Plant Species", ylab = "Butterfly Abundance") + 
+  annotate("text", x = 30, y = 95, label = bquote("IRR = " ~ .(round(exp(summary(tot_ab)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(tot_ab)$coefficients[2,2]), 2)))) + 
+  annotate("text", x = 30, y = 90, label = bquote("p-value = " ~ .(round(summary(tot_ab)$coefficients[2,4], 3)))) +
+  annotate("text", x = 30, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(tot_ab), 1 - deviance/null.deviance)), 2))))
+
+
 
 # Shannon
 shan_tot <- basic_plot(tot_sh, condition = "nspecies", 
                        dat = abp, x = nspecies, y = Shannon,
                        xlab = "Number of Flowering Plant Species", ylab = "Butterfly Shannon Diversity") + 
   annotate("text", x = 31, y = 8.5, label = bquote("IRR = " ~ .(round(summary(tot_sh)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(tot_sh)$coefficients[2,2], 2)))) + 
-  annotate("text", x = 31, y = 8, label = bquote("p-value = " ~ .(round(summary(tot_sh)$coefficients[2,4], 2)))) +
-  annotate("text", x = 31, y = 7.5, label = bquote(R^2 ~ " = "  ~ .(round(summary(tot_sh)$adj.r.squared , 2)))) 
+  annotate("text", x = 31, y = 8.2, label = bquote("p-value = " ~ .(round(summary(tot_sh)$coefficients[2,4], 2)))) +
+  annotate("text", x = 31, y = 7.9, label = bquote(R^2 ~ " = "  ~ .(round(summary(tot_sh)$adj.r.squared , 2)))) 
 
-tot <- (ab_tot_n + ab_tot) / shan_tot + 
-  plot_annotation(tag_levels = 'a', tag_suffix = ")") & 
-  plot_layout(guides = 'collect') & 
-  theme(legend.position = 'top') 
+tot <- (ab_tot_n + ab_tot_avg ) / shan_tot + 
+  plot_annotation(tag_levels = 'a', tag_suffix = ")")
 
 ggsave('figures/TotalBloom.png', tot, height = 10, width = 10, units = 'in', dpi = 450)
 
