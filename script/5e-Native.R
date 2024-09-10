dd0e01c95699198c8fa37594965b51dd9a37ac1c
icrichmond <52459005+icrichmond@users.noreply.github.com>
2024-09-10 16:02:03 -0400

    change panel c to slopes

--- script/5e-Native.R
+++ script/5e-Native.R
@@ -29,20 +29,27 @@ nat_sh <- readRDS('large/NatShann.rds')
 ab_nat_n <- basic_plot(nat_ab, condition = "nnative", 
                      dat = anp, x = nnative, y = Abundance,
                      xlab = "Number of Native Flowering Species", ylab = "Butterfly Abundance") + 
-  annotate("text", x = 12, y = 94, label = bquote("IRR = " ~ .(round(exp(summary(nat_ab)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(nat_ab)$coefficients[2,2]), 2)))) + 
-  annotate("text", x = 12, y = 90, label = bquote("p-value = " ~ .(round(summary(nat_ab)$coefficients[2,4], 2)))) +
-  annotate("text", x = 12, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(nat_ab), 1 - deviance/null.deviance)), 2)))) 
+  annotate("text", x = 11, y = 94, label = bquote("IRR = " ~ .(round(exp(summary(nat_ab)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(nat_ab)$coefficients[2,2]), 2)))) + 
+  annotate("text", x = 11, y = 90, label = bquote("p-value = " ~ .(round(summary(nat_ab)$coefficients[2,4], 15)))) +
+  annotate("text", x = 11, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(nat_ab), 1 - deviance/null.deviance)), 2)))) 
 
 ab_nat_avg <- basic_plot(nat_ab, condition = "avgnatbloom", 
                        dat = anp, x = avgnatbloom, y = Abundance,
                        xlab = "Average Native Bloom Cover", ylab = "Butterfly Abundance") + 
-  annotate("text", x = 12, y = 94, label = bquote("IRR = " ~ .(round(exp(summary(nat_ab)$coefficients[3,1]), 2)) ~ "+/-" ~ .(round(exp(summary(nat_ab)$coefficients[3,2]), 2)))) + 
-  annotate("text", x = 12, y = 90, label = bquote("p-value = " ~ .(round(summary(nat_ab)$coefficients[3,4], 2)))) +
-  annotate("text", x = 12, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(nat_ab), 1 - deviance/null.deviance)), 2)))) 
+  annotate("text", x = 10, y = 94, label = bquote("IRR = " ~ .(round(exp(summary(nat_ab)$coefficients[3,1]), 2)) ~ "+/-" ~ .(round(exp(summary(nat_ab)$coefficients[3,2]), 2)))) + 
+  annotate("text", x = 10, y = 90, label = bquote("p-value = " ~ .(round(summary(nat_ab)$coefficients[3,4], 9)))) +
+  annotate("text", x = 10, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(nat_ab), 1 - deviance/null.deviance)), 2)))) 
 
 
 ab_nat_int_n <- int_plot(nat_ab, list("nnative" = 5), condition = c("Niche.Breadth"), 
                          xlab = "", ylab = "Change in butterfly abundance with 5 species \nincrease in number of native flowering species")
+
+ab_nat_slope_n <- plot_slopes(nat_ab, variables = "nnative", condition = c("nnative", "Niche.Breadth" )) + 
+  theme_classic() + 
+  labs(fill = "", colour = "", x = "Number of Native Flowering Species") + 
+  scale_fill_viridis_d() + 
+  scale_colour_viridis_d() + 
+  theme(legend.position = 'top')
 
 ab_nat_int_avg <- int_plot(nat_ab, list("avgnatbloom" = 10), condition = c("Niche.Breadth"), 
                    xlab = "", ylab = "Change in butterfly abundance with 10% \nincrease in average native bloom cover")
@@ -58,7 +65,7 @@ sr_nat <- basic_plot(nat_n_sr, condition = "nnative",
 
 
 
-ab_nat <- (ab_nat_n + ab_nat_avg) / (ab_nat_int_n + ab_nat_int_avg) +
+ab_nat <- (ab_nat_n + ab_nat_avg) / (ab_nat_slope_n + ab_nat_int_avg) +
   plot_annotation(tag_levels = 'a', tag_suffix = ")")
 
 
