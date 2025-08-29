e4c26153b515259c0584b7cf15612db0fe96a096
icrichmond <bellarichmond@proton.me>
2025-08-29 12:23:56 -0400

    MASS

--- script/0-Packages.R
+++ script/0-Packages.R
@@ -1,6 +1,6 @@
 # Packages ----------------------------------------------------------------
 p <- c("data.table", "readr", "dplyr", "tidyr", "stringr", 
        "tibble", "purrr", "sf", "iNEXT", "ggplot2", "vegan",
-       "sjPlot", "marginaleffects", "performance", "forcats",
+       "sjPlot", "marginaleffects", "performance", "forcats", "MASS",
        "osmdata", "ggmap", "ggspatial", "patchwork", "modelsummary")
 lapply(p, library, character.only = T)
