b423a5643d6e60f714fce98f67c6150bf563f982
Bella Richmond <bellarichmond@proton.me>
2026-05-19 16:52:03 -0400

    updated fig

--- script/5d-Total.R
+++ script/5d-Total.R
@@ -4,9 +4,8 @@ source('script/function-IntPlot.R')
 
 
 # Data --------------------------------------------------------------------
-plant <- read.csv('output/PlantCleanbySite.csv') %>% 
-  mutate(Pond = str_remove(Pond, "SWF-"),
-         Pond = as.numeric(Pond))
+plant <- read.csv('output/PlantCleanbySite.csv') %>%
+  mutate(Pond = str_remove(Pond, "SWF-"), Pond = as.numeric(Pond))
 anthro <- st_read("output/AnthroFull.gpkg")
 niche <- read.csv('output/ButterflyNiche.csv')
 butt <- read.csv('output/ButterflyCleanbySite.csv')
@@ -18,43 +17,87 @@ abp <- inner_join(ab, plant, by = "Pond")
 
 
 # Models ------------------------------------------------------------------
-tot_ab <- readRDS('large/TotAbund.rds')
-tot_n_sr <- readRDS('large/TotNicheSR.rds')
-tot_sh <- readRDS('large/TotShann.rds')
-
+mod_ab_tot <- readRDS('large/TotAbund.rds')
+mod_n_sr_tot <- readRDS('large/TotNicheSR.rds')
+mod_sh_tot <- readRDS('large/TotShann.rds')
 
 
 # Total Bloom -------------------------------------------------------------
 
-# Abundance 
-ab_tot_avg <- basic_plot(tot_ab, condition = c("avgbloom"), colour = Niche.Breadth,
-                         dat = anp, x = avgbloom, y = Abundance,
-                       xlab = "Average Total Bloom Cover", ylab = "Butterfly Abundance") + 
-  annotate("text", x = 19, y = 95, label = bquote("IRR = " ~ .(round(exp(summary(tot_ab)$coefficients[3,1]), 2)) ~ "+/-" ~ .(round(exp(summary(tot_ab)$coefficients[3,2]), 2)))) + 
-  annotate("text", x = 19, y = 90, label = bquote("p-value = " ~ .(round(summary(tot_ab)$coefficients[3,4], 21)))) +
-  annotate("text", x = 19, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(tot_ab), 1 - deviance/null.deviance)), 2))))
+# Abundance
+
+# Abundance
+ab_cov <- basic_plot(
+  mod_ab_tot,
+  condition = c("avgbloom"),
+  dat = abp,
+  x = avgbloom,
+  y = abund,
+  xlab = "Average Total Bloom Cover (%)",
+  ylab = "Butterfly Abundance"
+)
 
 
-ab_tot_n <- basic_plot(tot_ab, condition = c("nspecies"), colour = Niche.Breadth,
-                       dat = anp,x = nspecies, y = Abundance,
-                       xlab = "Number of Flowering Plant Species", ylab = "Butterfly Abundance") + 
-  annotate("text", x = 30, y = 95, label = bquote("IRR = " ~ .(round(exp(summary(tot_ab)$coefficients[2,1]), 2)) ~ "+/-" ~ .(round(exp(summary(tot_ab)$coefficients[2,2]), 2)))) + 
-  annotate("text", x = 30, y = 90, label = bquote("p-value = " ~ .(round(summary(tot_ab)$coefficients[2,4], 3)))) +
-  annotate("text", x = 30, y = 86, label = bquote(R^2 ~ " = "  ~ .(round((with(summary(tot_ab), 1 - deviance/null.deviance)), 2))))
+# Species Richness
 
+sr_ntot <- basic_plot(
+  mod_n_sr_tot,
+  condition = c("nspecies"),
+  dat = abp,
+  x = nspecies,
+  y = SpeciesRichness,
+  xlab = "Number of Flowering Species",
+  ylab = "Butterfly Species Richness"
+)
+
+sr_cov <- basic_plot(
+  mod_n_sr_tot,
+  condition = c("avgbloom", "Niche.Breadth"),
+  colour = Niche.Breadth,
+  dat = abp,
+  x = avgbloom,
+  y = SpeciesRichness,
+  xlab = "Average Total Bloom Cover (%)",
+  ylab = "Butterfly Species Richness"
+)
 
 
 # Shannon
-shan_tot <- basic_plot(tot_sh, condition = "nspecies", 
-                       dat = abp, x = nspecies, y = Shannon,
-                       xlab = "Number of Flowering Plant Species", ylab = "Butterfly Shannon Diversity") + 
-  annotate("text", x = 31, y = 8.5, label = bquote("Estimate = " ~ .(round(summary(tot_sh)$coefficients[2,1], 2)) ~ "+/-" ~ .(round(summary(tot_sh)$coefficients[2,2], 2)))) + 
-  annotate("text", x = 31, y = 8.2, label = bquote("p-value = " ~ .(round(summary(tot_sh)$coefficients[2,4], 2)))) +
-  annotate("text", x = 31, y = 7.9, label = bquote(R^2 ~ " = "  ~ .(round(summary(tot_sh)$adj.r.squared , 2)))) 
+sh_ntot <- basic_plot(
+  mod_sh_tot,
+  condition = c("nspecies"),
+  dat = abp,
+  x = nspecies,
+  y = Shannon,
+  xlab = "Number of Flowering Species",
+  ylab = "Butterfly Shannon"
+)
 
-tot <- ab_tot_n + ab_tot_avg + shan_tot + guide_area() + 
-  plot_annotation(tag_levels = 'a', tag_suffix = ")") +
-  plot_layout(guides = 'collect', nrow = 2)
+sh_cov <- basic_plot(
+  mod_sh_tot,
+  condition = c("avgbloom"),
+  dat = abp,
+  x = avgbloom,
+  y = Shannon,
+  xlab = "Average Total Bloom Cover (%)",
+  ylab = "Butterfly Shannon"
+)
 
-ggsave('figures/TotalBloom.png', tot, height = 10, width = 10, units = 'in', dpi = 450)
 
+tot <- sr_ntot +
+  sr_cov +
+  sh_ntot +
+  sh_cov +
+  ab_cov +
+  plot_layout(ncol = 2, nrow = 3, guides = 'collect') &
+  theme(legend.position = "top")
+
+# save
+ggsave(
+  'figures/TotalBloom.png',
+  tot,
+  height = 10,
+  width = 10,
+  units = 'in',
+  dpi = 450
+)
